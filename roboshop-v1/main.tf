d8c6bb1d5b96124bb427396448c1850a524f990a
vishnureddy <tarigopula34@gmail.com>
2024-10-22 17:45:55 +0530

    Fronted terraform first code executed.

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "fronted" {
   ami      = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["i-0116178178774deb6"]
+  vpc_security_group_ids= ["sg-0925e3ad9a29b3b88"]
   tags = {
     name= "fronted"
 
