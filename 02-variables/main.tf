e4e3f94cc244ca7cc73d7afbfdacdebdf5c1ed01
vishnureddy <tarigopula34@gmail.com>
2024-10-23 17:09:08 +0530

    The outputs on terraform

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -13,3 +13,46 @@ output "URL" {
 output "URL1" {
   value = "URL - ${var.URL}"
 }
+
+#Data types
+variable "num1" {
+  default = true
+}
+variable "bool1" {
+  default = true
+}
+variable "string1" {
+  default = "Hello"
+}
+
+#number,booleans,string
+# variable type
+
+variable "string2" {
+  default = "world"
+}
+variable "list1" {
+  default = [
+    10,
+    20,
+    "xyz",
+    false
+  ]
+}
+#list can have combination of multiple data types
+variable "map1" {
+  default = {
+    aws = {
+      trainer="John"
+      duration=30
+    }
+
+    }
+  }
+output "list_1" {
+  value = var.list1[2]
+}
+
+output "map1_aws_trainer" {
+  value = var.map1["aws"]["trainer"]
+}
