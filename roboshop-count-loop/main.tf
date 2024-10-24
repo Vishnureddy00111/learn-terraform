68f5ab630a63bc6bf6285327739c34b15a43550d
vishnureddy <tarigopula34@gmail.com>
2024-10-24 16:14:26 +0530

    The for_each function is executing .

--- roboshop-count-loop/main.tf
+++ roboshop-count-loop/main.tf
@@ -15,7 +15,7 @@ variable "instance" {
 }
 
 
-resource "aws_instance" "fronted" {
+resource "aws_instance" "instance" {
   count = length(var.instance)
   ami      = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
