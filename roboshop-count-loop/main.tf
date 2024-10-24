93f48fb1c78c71f4bf9b009d2750d29437ebd4e8
vishnureddy <tarigopula34@gmail.com>
2024-10-24 15:18:24 +0530

    The for_each function is executing .

---
+++ roboshop-count-loop/main.tf
@@ -0,0 +1,35 @@
+variable "instance" {
+  default = [
+    "fronted",
+    "mongodb",
+    "catalogue",
+    "user",
+    "robbitmq",
+    "mysql",
+    "shipping",
+    "payment",
+    "dispatch",
+    "reids",
+    "cart"
+  ]
+}
+
+
+resource "aws_instance" "fronted" {
+  count = length(var.instance)
+  ami      = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids= ["sg-0925e3ad9a29b3b88"]
+  tags = {
+    name= var.instance[count.index]
+
+  }
+}
+
+resource "aws_route53_record" "fronted" {
+  zone_id = "Z03892523DTYJ3OLXCS7D"
+  name    = "$(var.instance[count.index])-dev.vishnuredddy.online"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.fronted.private_ip]
+}
