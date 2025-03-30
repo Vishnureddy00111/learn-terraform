ef79e6c9bfa40228d29673246ba31900e8f15288
vishnureddy <tarigopula34@gmail.com>
2025-03-30 08:30:55 +0530

    terraform resources scripts have been executed successfully

--- 04-data/main.tf
+++ 04-data/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
-  ami      = "ami-09c813fb71547fc4f"
+  ami      = "data.aws_ami.ami.id"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["data.aws.security_group.sg.id"]
+  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
   tags = {
     name= "test"
 
