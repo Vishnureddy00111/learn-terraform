eabc98dcff2358ae5f21c20f1d01f7092c727804
vishnureddy <tarigopula34@gmail.com>
2025-04-02 15:57:26 +0530

    enviroment core stracture

---
+++ 09-vault/main.tf
@@ -0,0 +1,18 @@
+provider "vault" {
+  address = "http://172.31.80.62:8200"
+  token = "var.vault_token"
+  skip_tls_verify: "true"
+    }
+
+
+variable "vault_token" {}
+
+data "vault_kv_secret_v2" "example" {
+  mount = "test"
+  name  = "my_credentials"
+}
+
+resource "local_file" "foo" {
+  content  = "data.vault_kv_secret_v2.example.data_json"
+  filename = "/tmp/secret/"
+}
