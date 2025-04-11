e9aa098f6e430dba1b3b2282bb1544448f505794
vishnureddy <tarigopula34@gmail.com>
2025-04-11 17:27:57 +0530

    vault

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -6,22 +6,22 @@ provider "vault" {
 
 variable "vault_token" {}
 
-# data "vault_generic_secret" "example" {
-#   path = "test/my_credentials"
-# }
-
-data "vault_kv_secret_v2" "example" {
-  name  = "my_credentials"
-  mount = "test"
+data "vault_generic_secret" "example" {
+  path = "test/my_credentials"
 }
 
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
+# }
+
 resource "local_file" "foo" {
-  content  = data.vault_kv_secret.v2.example.data_json["password"]
+  content  = data.vault_generic_secret.example.data["password"]
   filename = "/tmp/secret"
 }
 
-output "json" {
-  value = data.vault_kv_secret_v2.example.data_json
-}
+# output "json" {
+#   value = data.vault_kv_secret_v2.example.data_json
+# }
 
 
