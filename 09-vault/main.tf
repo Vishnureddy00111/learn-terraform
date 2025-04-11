1a3eeb1e6409553ffde75f56cf1f1da2a36b070c
vishnureddy <tarigopula34@gmail.com>
2025-04-11 13:11:45 +0530

    vault

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -1,24 +1,27 @@
 provider "vault" {
   address = "https://172.31.80.62:8200"
-  token = "var.vault_token"
-  skip_tls_verify: "true"
-    }
-
+  token   = var.vault_token
+  skip_tls_verify = true
+}
 
 variable "vault_token" {}
 
-data "vault_kv_secret_v2" "example" {
-  mount = "test"
-  name  = "my_credentials"
+data "vault_generic_secret" "example" {
+  path = "test/my_credentials"
 }
 
-# resource "local_file" "foo" {
-#   content  = "(data.vault_kv_secret_v2.example.data_json)"
-#   filename = "/tmp/secret/"
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
 # }
-#
+
+# resource "local_file" "foo" {
+#   content  = data.vault_generic_secret.example.data["password"]
+#   filename = "/tmp/secret"
+# }
+
 # output "json" {
-#   value = "data.vault_kv_secret_v2.example.data_json"
+#   value = data.vault_kv_secret_v2.example.data_json
 # }
 
 
